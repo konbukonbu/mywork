93dd8b7ac2049ef458747af4ae3fabb41dd57cc4
adachi <adachi@DESKTOP-VQ328T7>
2018-05-01 10:11:27 +0900

    練習用にコミット

---
+++ HelloWorld/src/y2018/HelloWorld.java
@@ -0,0 +1,14 @@
+package y2018;
+
+/**
+ * 練習用のプロジェクト
+ *
+ * @author adachi
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
