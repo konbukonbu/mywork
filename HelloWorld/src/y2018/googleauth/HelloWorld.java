388cc3cb2da630cd9d359b2ea58c8ef82d2fd9e6
adachi <adachi@DESKTOP-VQ328T7>
2018-07-02 06:03:30 +0900

    googleAuthサンプルやねん

---
+++ HelloWorld/src/y2018/googleauth/HelloWorld.java
@@ -0,0 +1,25 @@
+package y2018.googleauth;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+
+/**
+ * @see https://blog.ohgaki.net/use-2-factor-authentication-with-your-web-sites
+ * @see https://www.slideshare.net/hatanakaakihiro/ss-53907884
+ * @see https://qiita.com/yuizho/items/553f1aac138143d8f8d9
+ *
+ */
+public class HelloWorld {
+  public static final int CONST = 1;
+
+  public static void main(String[] args) throws Exception {
+    //		AuthyApiClient client = new AuthyApiClient("73c2fe32-ffd7-4b37-acf7-ebbaefa110b3");
+    //		System.out.println(client.getTokens().verify(123456, "123456"));
+
+    GoogleAuthenticator gAuth = new GoogleAuthenticator();
+    gAuth.setCredentialRepository(new MyCredentialRepository());
+    GoogleAuthenticatorKey key = gAuth.createCredentials("73c2fe32-ffd7-4b37-acf7-ebbaefa110b3");
+    System.out.println(key.getVerificationCode());
+  }
+
+}
---
+++ HelloWorld/src/y2018/googleauth/MyCredentialRepository.java
@@ -0,0 +1,19 @@
+package y2018.googleauth;
+
+import java.util.List;
+
+import com.warrenstrange.googleauth.ICredentialRepository;
+
+public class MyCredentialRepository implements ICredentialRepository {
+
+  @Override
+  public String getSecretKey(String arg0) {
+    // TODO 自動生成されたメソッド・スタブ
+    return null;
+  }
+
+  @Override
+  public void saveUserCredentials(String arg0, String arg1, int arg2, List<Integer> arg3) {
+    // TODO 自動生成されたメソッド・スタブ
+  }
+}
