aaea2b0436b3f26ea98911106d6147fb427245a0
adachi <tkbfj573@yahoo.co.jp>
2019-06-30 09:48:49 +0900

    #6 課題No7を解いたぜよ🐧

---
+++ Java100Nock/src/practice_01_10/p07/ExcecuteWithLib.java
@@ -0,0 +1,17 @@
+package practice_01_10.p07;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ExcecuteWithLib {
+
+  public static void main(String[] args) {
+    String str = "";
+    if (StringUtils.isEmpty(str)) {
+      System.out.println("str is Empty!!");
+    } else {
+      System.out.println("str is not Empty");
+    }
+
+  }
+
+}
